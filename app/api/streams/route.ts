46cf5e06542deeeea9962b9d17307983e2913c3f
Niteesh Kulhari <nkulhari96@gmail.com>
2024-10-17 12:08:50 -0400

    Updated routes

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -1,49 +1,78 @@
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
-import { prismaClient } from "@/app/lib/db"
+import { prismaClient } from "@/app/lib/db";
 
 // Youtube regex
-const YT_REGEX = new RegExp("^https:\/\/www\.youtube\.com\/watch\?v=[\w-]{11}")
+const YT_REGEX = new RegExp("^https://www.youtube.com/watch?v=[w-]{11}");
 
 // Zod Schema to validate data
 const CreateStreamSchema = z.object({
-    creatorId: z.string(),
-    url: z.string()
-})
+  creatorId: z.string(),
+  url: z.string(),
+});
 
+export async function POST(req: NextRequest) {
+  try {
+    // Validating data through zod
+    const data = CreateStreamSchema.parse(await req.json());
+    // checking link with the help of regex
+    const isYt = YT_REGEX.test(data.url);
 
-export async function POST(req: NextRequest){
-
-    try {
-        // Validating data through zod
-        const data = CreateStreamSchema.parse(await req.json());
-        // checking link with the help of regex
-        const isYt = YT_REGEX.test(data.url);
-
-        if(!isYt){
-            return NextResponse.json({
-                message: "Wrong URL Format"
-            }, {
-                status: 411
-            })
+    if (!isYt) {
+      return NextResponse.json(
+        {
+          message: "Wrong URL Format",
+        },
+        {
+          status: 411,
         }
-
-        const extractedId = data.url.split("?v=")[1];
-
-        prismaClient.stream.create({
-            data: {
-                userId: data.creatorId,
-                url: data.url,
-                extractedId: extractedId,
-                type: "Youtube"
-            }
-        })
-    } catch (error) {
-        return NextResponse.json({
-            message: "Error while adding a stream"
-        }, {
-            status: 411
-        })
+      );
     }
 
+    const extractedId = data.url.split("?v=")[1];
+
+    prismaClient.stream.create({
+      data: {
+        userId: data.creatorId,
+        url: data.url,
+        extractedId: extractedId,
+        type: "Youtube",
+      },
+    });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "Error while adding a stream",
+      },
+      {
+        status: 411,
+      }
+    );
+  }
+}
+
+export async function GET(req: NextRequest) {
+  const creatorId = req.nextUrl.searchParams.get("creatorId");
+
+  //   if (!creatorId) {
+  //     return NextResponse.json({
+  //       message: "Please provide the creator Id",
+  //     });
+  //   }
+
+  try {
+    const streams = await prismaClient.stream.findMany({
+      where: {
+        userId: creatorId ?? "",
+      },
+    });
+
+    return NextResponse.json({
+      streams,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      message: "Error in finding all the streams",
+    });
+  }
 }
