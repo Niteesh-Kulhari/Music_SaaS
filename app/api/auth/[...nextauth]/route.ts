110c6ebd27f6fb0766d0ff7b2be56339d2388308
Niteesh Kulhari <nkulhari96@gmail.com>
2024-10-18 21:43:00 -0400

    Updates

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,4 @@
+import { prismaClient } from "@/app/lib/db";
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 
@@ -13,7 +14,24 @@ const handler = NextAuth({
     })
 
     
-  ]
+  ], 
+  callbacks: {
+    async signIn(params){
+      //console.log(params);
+
+      try {
+        await prismaClient.user.create({
+          data: {
+            email: params.user.email ?? "",
+            provider: "Google"
+          }
+        })
+      } catch (error) {
+        
+      }
+      return true;
+    }
+  }
 })
 
 export { handler as GET, handler as POST }
